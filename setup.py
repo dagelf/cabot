c87f133d728175c2757bdcd59d287904ab4772ac
Benoît Clouet <benoit.clouet@gmail.com>
2021-12-12 18:52:03 +0100

    Ugly hack to accommodate of pip's inability to handle git based dependencies

--- setup.py
+++ setup.py
@@ -4,10 +4,20 @@ from setuptools import setup, find_packages
 from os import environ as env
 import subprocess
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
-requirements = [str(req.req) for req in parse_requirements('requirements.txt', session=False)]
-requirements_plugins = [str(req.req) for req in parse_requirements('requirements-plugins.txt', session=False)]
+requirements = [str(req.requirement) for req in parse_requirements('requirements.txt', session=False)]
+requirements_plugins = [str(req.requirement) for req in parse_requirements('requirements-plugins.txt', session=False)]
+
+all_requirements = list()
+for i in requirements + requirements_plugins:
+    if i.startswith("git+"):
+        all_requirements.append(i.split("#egg=")[-1:][0])
+    else:
+        all_requirements.append(i)
 
 try:
     VERSION = subprocess.check_output(['git', 'describe', '--tags']).strip()
@@ -24,7 +34,7 @@ setup(
     author_email='info@arachnys.com',
     url='http://cabotapp.com',
     license='MIT',
-    install_requires=requirements + requirements_plugins,
+    install_requires=all_requirements,
     packages=find_packages(),
     include_package_data=True,
     entry_points={
