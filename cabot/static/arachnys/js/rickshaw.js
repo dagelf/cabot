5a997a570116411d65fe8cc6d52e030f7779cc29
Benoît Clouet <benoit.clouet@gmail.com>
2021-12-27 11:51:20 +0100

    What a nice time to be alive !

--- cabot/static/arachnys/js/rickshaw.js
+++ cabot/static/arachnys/js/rickshaw.js
@@ -2019,7 +2019,7 @@ Rickshaw.Graph.HoverDetail = Rickshaw.Class.create({
     var graph = this.graph = args.graph;
 
     this.xFormatter = args.xFormatter || function(x) {
-      return new Date( x * 1000 ).toLocaleString();
+      return new Date( x * 1000 ).toString();
     };
 
     this.yFormatter = args.yFormatter || function(y) {
