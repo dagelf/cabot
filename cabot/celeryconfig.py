ab55539cee819a5a271e212c46ac95f3f930986e
Benoît Clouet <benoit.clouet@gmail.com>
2022-01-01 17:23:31 +0100

    Add ability to use TLS for connecting to the broker

--- cabot/celeryconfig.py
+++ cabot/celeryconfig.py
@@ -3,8 +3,9 @@ import os
 from cabot.settings_utils import environ_get_list
 
 broker_url = environ_get_list(["CELERY_BROKER_URL", "CACHE_URL"])
-# Set environment variable if you want to run tests without a redis instance
+broker_use_ssl = os.environ.get("CELERY_BROKER_USE_SSL", False)
 
+# Set environment variable if you want to run tests without a redis instance
 task_always_eager = environ_get_list(
     ["CELERY_ALWAYS_EAGER", "CELERY_TASK_ALWAYS_EAGER"], False
 )
