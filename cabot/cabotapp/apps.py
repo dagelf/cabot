f9b9e8e21e4217c0968100eced76618321d2a785
Benoît Clouet <benoit.clouet@gmail.com>
2022-01-02 20:28:40 +0100

    Ooops !

--- cabot/cabotapp/apps.py
+++ cabot/cabotapp/apps.py
@@ -1,5 +1,11 @@
+import os
+
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
+
+from cabot.celery import app
+
+running_workers = dict()
 
 
 def post_migrate_callback(**kwargs):
@@ -15,3 +21,30 @@ class CabotappConfig(AppConfig):
 
     def ready(self):
         post_migrate.connect(post_migrate_callback, sender=self)
+
+        # Only run in application server context
+        is_running_in_server_context = bool(os.environ.get("IS_WEBSERVER", False))
+        if is_running_in_server_context:
+            print(">>>> Registering events")
+
+            def announce_worker_online(event):
+                print(f"announce_worker_online {event}")
+                running_workers[event["hostname"]] = event
+                print(f"Add {running_workers}")
+
+            def announce_worker_offline(event):
+                print(f"announce_worker_offline {event}")
+                event["hostname"] in running_workers and running_workers.pop(
+                    event["hostname"]
+                )
+                print(f"Remove {running_workers}")
+
+            with app.connection() as connection:
+                recv = app.events.Receiver(
+                    connection,
+                    handlers={
+                        "worker-online": announce_worker_online,
+                        "worker-offline": announce_worker_offline,
+                    },
+                )
+                recv.capture(limit=None, timeout=None, wakeup=True)
