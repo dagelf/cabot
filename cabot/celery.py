56ddf6fc65f9554110d44ef8fa1895b439252d77
Benoît Clouet <benoit.clouet@gmail.com>
2022-01-02 21:02:27 +0100

    Revert the mess and try to use an existing solution

--- cabot/celery.py
+++ cabot/celery.py
@@ -9,8 +9,6 @@ app = Celery("cabot")
 app.config_from_object("cabot.celeryconfig")
 app.autodiscover_tasks()
 
-running_workers = dict()
-
 app.conf.beat_schedule = {
     "run-all-checks": {
         "task": "cabot.cabotapp.tasks.run_all_checks",
@@ -25,29 +23,3 @@ app.conf.beat_schedule = {
         "schedule": timedelta(seconds=60 * 60 * 24),
     },
 }
-
-# Only run in application server context
-is_running_in_server_context = bool(os.environ.get("IS_WEBSERVER", False))
-
-if is_running_in_server_context:
-    print(">>>> Registering events")
-
-    def announce_worker_online(event):
-        print(f"announce_worker_online {event}")
-        running_workers[event["hostname"]] = event
-        print(f"Add {running_workers}")
-
-    def announce_worker_offline(event):
-        print(f"announce_worker_offline {event}")
-        event["hostname"] in running_workers and running_workers.pop(event["hostname"])
-        print(f"Remove {running_workers}")
-
-    with app.connection() as connection:
-        recv = app.events.Receiver(
-            connection,
-            handlers={
-                "worker-online": announce_worker_online,
-                "worker-offline": announce_worker_offline,
-            },
-        )
-        recv.capture(limit=None, timeout=None, wakeup=True)
