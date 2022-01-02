9137d942f4c4677e255bb4b981dc3d9a0e433ddd
Benoît Clouet <benoit.clouet@gmail.com>
2022-01-02 02:35:21 +0100

    Try to find a way to trigger event registration only at app startup

--- cabot/cabotapp/apps.py
+++ cabot/cabotapp/apps.py
@@ -15,6 +15,3 @@ class CabotappConfig(AppConfig):
 
     def ready(self):
         post_migrate.connect(post_migrate_callback, sender=self)
-        from ..celery import register_events
-
-        register_events()
